b18879c7f7d4214fc45fda61bb60682d544033d1
Esa Salminen <esa_salminen@hotmail.com>
2022-01-26 20:24:23 +0200

    Hienosäätöä

--- exercise1/src/components/FakenewsJSON.js
+++ exercise1/src/components/FakenewsJSON.js
@@ -2,22 +2,22 @@ export const SmallnewsGroup = [
     {
         no: 1,
         title: "Vantaa",
-        text:"Vantaalle nousi vankilan näköinen päiväkoti, jota pilkataan surutta verkossa. Arkkitehti on järkyttynyt saamastaan kohtelusta"
+        text:"Oli kirkolle ota loi pitkalle jostakin lasianne tuomista. Vie sina vai itku muka onko joka aija. Mina omat et olin et olet en me raha. Ei anna sala en on mina joka. Merirosvo saaliista varmuuden et on nalkainen kalliolla ne kerrankin. Huulet en suuren no kuussa en te. Onhan ela osa pappa oikea tuoma syyni kun tekin."
     },
     {
         no: 2,
         title: "Rikosepäilyt", 
-        text: "EIT-huijaus: Kuolleeksi väitetyn irakilaismiehen tytär valehteli perheen taustoista jo turvapaikkahakemuksessa"
+        text: "Ei jo jatkaessa antamatta saaliista ne jurottava he. Ainoalla toisessa moittiva nyt tuhansia ole. Naemme ja se teidan vapaus ei et meille. Tuo nyt kyllahan kun niinkuin markalla. Ruuhen pullon lautaa ja on hiukan raakun. Tassakaan ero pysahtyen kun valmiiksi muutamata sen kappaleen sammuttaa."
     },
     {
         no: 3,
         title: "Päivittyvä seuranta",
-        text: "STM: Parasetamolia tai deksametasonia sisältävien lääkkeiden myyntiä rajoitetaan - Ministeriö haluaa varmistaa valmisteiden riittävyyden"
+        text: "Naki raha mene omaa aave tuli ai se on. Eli konsti kas silmaa heidat tahdon jaa vaanii tyhjaa. Han puskisi hymyssa melkein vei. Saari yhden olkaa maata vai ole terve viime. Pianaikaa jumalalta korjannut nauroivat ja no ai kerrankin laineilla on. Ulapalle niinkaan tarkoita ole sen lekkerin iso oletteko jaa. Oma paljonkos saa pysahtyen eli koettelen"
     },
     {
         no: 4,
         title: "Nyt.fi",
-        text: "Harry Styles puki ylleen suomalaisen suunnittelijan neuleen, nyt tätä 'mummon tilkkutäkkiä' neulovat kymmenet tuhannet ympäri maailman - Soitimme vaatesuunnittelijalle"
+        text: "Niin paha ne en olet. Vastapaata no ei rantasipit pirullisen. Vie nakyi osa syyni tee yha asiaa. Tai lahtoa vei ylihan elanyt senkin tai. Viina annat onhan no ai oliko. Tee vahinkoa uskotaan kaikille aittanne kas tarkoita ela jai vedappas."
     }
 
 ]
@@ -25,19 +25,19 @@ export const SmallnewsGroup = [
 export const ContentbarGroup = [
     {
         type: "News",
-        text: "SÄÄSTÖVINKKI: Harriet Rabb tajusi avioliiton taloudelliset hyödyt ja kosi miljoonääriä"
+        text: "ULKOMAAT: Yhdysvallat jättänyt kirjallisen vastauksen Venäjän turvallisuusvaatimuksiin - Naton pääsihteerin tiedotustilaisuus suorana noin kello 20"
     },
     {
         type: "News",
-        text: "IHMISEN ANATOMIA: Kuinka paljon paskassa voi olla kultaa?"
+        text: "KORONATILANNE: Tehohoitoa vaativien korona­potilaiden määrässä on tapahtunut merkittävä käänne parempaan"
     },
     {
         type: "News",
-        text: "TERVEYS JA HYVINVOINTI: Mika Rämet kertoo: Kuudestoista rokote antaa pysyvän suojan"
+        text: "ELOKUVAT: Laajasti ihastellulle Hytti nro 6:lle tyrmäävä kritiikki The New Yorker -lehdessä – Toisen suuren lehden kriitikolta vastaavasti kehut"
     },
     {
         type: "",
-        text: "MAINOS: Sydänkohtaukset kasvussa. Hautaa sinäkin sukusi ryhmä-alennuksella Keijon Ruumishuoneella"
+        text: "MAINOS: Tutustu Hesariin 2 viikkoa maksutta"
     }
 ]
 
@@ -46,24 +46,24 @@ export const ArticleGroup = [
         title: "Koronavirus",
         imgSrc: "/img/korona.png",
         alt: "korona",
-        text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci. Aenean nec lorem. In porttitor. Donec laoreet nonummy augue. Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.",
+        text: "Et eiko tama ei asia. Talvella saa voi talveksi kaantyen tuo. Rusinoita kenenkaan seitsemia tai mihinkaan laakariin kas. Jai ota ajaakos nuo nuorten lempean viinaan huoneen tulitte. Kayn en niin ei saat tein. Paaltapain hyvastinsa te ne hiljaisuus sinullekin.",
     },
     {
         title: "Maailmanloppu",
         imgSrc: "/img/maailmanloppu.jpg",
         alt: "maailmanloppu",
-        text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci. Aenean nec lorem. In porttitor. Donec laoreet nonummy augue. Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.",
+        text: "Se et taskuun ukkikin enempaa kalassa. Ja oikeastaan lintukarit se tuommoinen tarkastaen huumauksen seuratessa. Peri sano jai kay vai osaa ihan. Ihan elaa puhe he mika ne sata. Toden et se ne heita on ettei. Teita loppu istui kas ilkea ostaa kay. Pera omaa toki voi ukko ole aani.",
     },
     {
         title: "Zombiehyökkäys",
         imgSrc: "/img/zombieattack.png",
         alt: "zombie",
-        text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci. Aenean nec lorem. In porttitor. Donec laoreet nonummy augue. Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.",
+        text: "Riittaisi huumannut ai millainen ja pianaikaa. Totisesti mihinkaan toi eri tai jos ainaisena huumannut teistakin. Ja moisi siina en kaula joita. Ero tee ulos jaa enko mene. Ylpeammin ai jo juoksette maistanut ja ajaisivat. Maailman uskotaan me maailman ai poikansa toimitan se kutukala on. Voi kai porsas kaikin nakisi. Ukkovaari et tahtoisin no kerrankin kuunnella ai seitsemia he hiljainen. Olet vain vai toi alla ela.",
     },
     {
         title: "Maankuori on revennyt",
         imgSrc: "/img/maankuori.jpg",
         alt: "maankuori",
-        text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci. Aenean nec lorem. In porttitor. Donec laoreet nonummy augue. Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.",
+        text: "Saa vatsa mista hyvat silla uhria oli. Puhuu arisi en ai syyni ne joita ja. On ja rihmoihin tarpeemme kerrankin mimmoinen. Ole ole venheen tuo vuosiin sentaan aikomus ota. Tuommoinen et kalapaikat lainattiin ja on on pannaksesi. Porstuan he israelin se isastasi ilmestys on selvalla tyynessa. Sai ero oma hupaisa ummessa naapuri mistaan ennenko jalasta ota. Han palkinnon semmoisen saa jaa raastavat nyt. Yhta et emma me mene no mies eiko ne.",
     }
 ]
