1f78037b3eb3fb1948cafded25eb898aff4820d7
Esa Salminen <esa_salminen@hotmail.com>
2022-02-06 16:18:18 +0200

    ex4 products api finished

--- exercise4/index.js
+++ exercise4/index.js
@@ -5,24 +5,275 @@ const port = 3000;
 
 let products = [
     {
-        id: 'a3b70433-b2c9-428d-8a03-061a780566c9',
-        name: "product 1",
-        manufacturer: "man1",
-        category: "cat1",
-        description: "desc1",
-        price: "1",
-        imageUrl: "http://image.jpg"
+    id: 1,
+    name: "iPhone 9",
+    description: "An apple mobile which is nothing like apple",
+    price: 549,
+    manufacturer: "Apple",
+    category: "smartphones",
+    imageUrl: "https://dummyjson.com/image/i/products/1/thumbnail.jpg",
     },
     {
-        id: 'a3b70433-h4c9-428d-8a03-061a780566c9',
-        name: "product 2",
-        manufacturer: "man2",
-        category: "cat2",
-        description: "desc2",
-        price: "2",
-        imageUrl: "http://image2.jpg"
+    id: 2,
+    name: "iPhone X",
+    description: "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
+    price: 899,
+    manufacturer: "Apple",
+    category: "smartphones",
+    imageUrl: "https://dummyjson.com/image/i/products/2/thumbnail.jpg",
+    },
+    {
+    id: 3,
+    name: "Samsung Universe 9",
+    description: "Samsung's new variant which goes beyond Galaxy to the Universe",
+    price: 1249,
+    manufacturer: "Samsung",
+    category: "smartphones",
+    imageUrl: "https://dummyjson.com/image/i/products/3/thumbnail.jpg",
+    },
+    {
+    id: 4,
+    name: "OPPOF19",
+    description: "OPPO F19 is officially announced on April 2021.",
+    price: 280,
+    manufacturer: "OPPO",
+    category: "smartphones",
+    imageUrl: "https://dummyjson.com/image/i/products/4/thumbnail.jpg",
+    },
+    {
+    id: 5,
+    name: "Huawei P30",
+    description: "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.",
+    price: 499,
+    manufacturer: "Huawei",
+    category: "smartphones",
+    imageUrl: "https://dummyjson.com/image/i/products/5/thumbnail.jpg",
+    },
+    {
+    id: 6,
+    name: "MacBook Pro",
+    description: "MacBook Pro 2021 with mini-LED display may launch between September, November",
+    price: 1749,
+    manufacturer: "APPle",
+    category: "laptops",
+    imageUrl: "https://dummyjson.com/image/i/products/6/thumbnail.png",
+    },
+    {
+    id: 7,
+    name: "Samsung Galaxy Book",
+    description: "Samsung Galaxy Book S (2020) Laptop With Intel Lakefield Chip, 8GB of RAM Launched",
+    price: 1499,
+    manufacturer: "Samsung",
+    category: "laptops",
+    imageUrl: "https://dummyjson.com/image/i/products/7/thumbnail.jpg",
+    },
+    {
+    id: 8,
+    name: "Microsoft Surface Laptop 4",
+    description: "Style and speed. Stand out on HD video calls backed by Studio Mics. Capture ideas on the vibrant touchscreen.",
+    price: 1499,
+    manufacturer: "Microsoft Surface",
+    category: "laptops",
+    imageUrl: "https://dummyjson.com/image/i/products/8/thumbnail.jpg",
+    },
+    {
+    id: 9,
+    name: "Infinix INBOOK",
+    description: "Infinix Inbook X1 Ci3 10th 8GB 256GB 14 Win10 Grey – 1 Year Warranty",
+    price: 1099,
+    manufacturer: "Infinix",
+    category: "laptops",
+    imageUrl: "https://dummyjson.com/image/i/products/9/thumbnail.jpg",
+    },
+    {
+    id: 10,
+    name: "HP Pavilion 15-DK1056WM",
+    description: "HP Pavilion 15-DK1056WM Gaming Laptop 10th Gen Core i5, 8GB, 256GB SSD, GTX 1650 4GB, Windows 10",
+    price: 1099,
+    manufacturer: "HP Pavilion",
+    category: "laptops",
+    imageUrl: "https://dummyjson.com/image/i/products/10/thumbnail.jpeg",
+    },
+    {
+    id: 11,
+    name: "perfume Oil",
+    price: 13,
+    manufacturer: "Impression of Acqua Di Gio",
+    category: "fragrances",
+    imageUrl: "https://dummyjson.com/image/i/products/11/thumbnail.jpg",
+    },
+    {
+    id: 12,
+    name: "Brown Perfume",
+    description: "Royal_Mirage Sport Brown Perfume for Men & Women - 120ml",
+    price: 40,
+    manufacturer: "Royal_Mirage",
+    category: "fragrances",
+    imageUrl: "https://dummyjson.com/image/i/products/12/thumbnail.jpg",
+    },
+    {
+    id: 13,
+    name: "Fog Scent Xpressio Perfume",
+    description: "Product details of Best Fog Scent Xpressio Perfume 100ml For Men cool long lasting perfumes for Men",
+    price: 13,
+    manufacturer: "Fog Scent Xpressio",
+    category: "fragrances",
+    imageUrl: "https://dummyjson.com/image/i/products/13/thumbnail.webp",
+    },
+    {
+    id: 14,
+    name: "Non-Alcoholic Concentrated Perfume Oil",
+    description: "Original Al Munakh® by Mahal Al Musk | Our Impression of Climate | 6ml Non-Alcoholic Concentrated Perfume Oil",
+    price: 120,
+    manufacturer: "Al Munakh",
+    category: "fragrances",
+    imageUrl: "https://dummyjson.com/image/i/products/14/thumbnail.jpg",
+    },
+    {
+    id: 15,
+    name: "Eau De Perfume Spray",
+    description: "Genuine Al-Rehab spray perfume from UAE/Saudi Arabia/Yemen High Quality",
+    price: 30,
+    manufacturer: "Lord - Al-Rehab",
+    category: "fragrances",
+    imageUrl: "https://dummyjson.com/image/i/products/15/thumbnail.jpg",
+    },
+    {
+    id: 16,
+    name: "Hyaluronic Acid Serum",
+    description: "L'OrÃ©al Paris introduces Hyaluron Expert Replumping Serum formulated with 1.5% Hyaluronic Acid",
+    price: 19,
+    manufacturer: "L'Oreal Paris",
+    category: "skincare",
+    imageUrl: "https://dummyjson.com/image/i/products/16/thumbnail.jpg",
+    },
+    {
+    id: 17,
+    name: "Tree Oil 30ml",
+    description: "Tea tree oil contains a number of compounds, including terpinen-4-ol, that have been shown to kill certain bacteria,",
+    price: 12,
+    manufacturer: "Hemani Tea",
+    category: "skincare",
+    imageUrl: "https://dummyjson.com/image/i/products/17/thumbnail.jpg",
+    },
+    {
+    id: 18,
+    name: "Oil Free Moisturizer 100ml",
+    description: "Dermive Oil Free Moisturizer with SPF 20 is specifically formulated with ceramides, hyaluronic acid & sunscreen.",
+    price: 40,
+    manufacturer: "Dermive",
+    category: "skincare",
+    imageUrl: "https://dummyjson.com/image/i/products/18/thumbnail.jpg",
+    },
+    {
+    id: 19,
+    name: "Skin Beauty Serum.",
+    description: "Product name: rorec collagen hyaluronic acid white face serum riceNet weight: 15 m",
+    price: 46,
+    manufacturer: "ROREC White Rice",
+    category: "skincare",
+    imageUrl: "https://dummyjson.com/image/i/products/19/thumbnail.jpg",
+    },
+    {
+    id: 20,
+    name: "Freckle Treatment Cream- 15gm",
+    description: "Fair & Clear is Pakistan's only pure Freckle cream which helpsfade Freckles, Darkspots and pigments. Mercury level is 0%, so there are no side effects.",
+    price: 70,
+    manufacturer: "Fair & Clear",
+    category: "skincare",
+    imageUrl: "https://dummyjson.com/image/i/products/20/thumbnail.jpg",
+    },
+    {
+    id: 21,
+    name: "- Daal Masoor 500 grams",
+    description: "Fine quality Branded Product Keep in a cool and dry place",
+    price: 20,
+    manufacturer: "Saaf & Khaas",
+    category: "groceries",
+    imageUrl: "https://dummyjson.com/image/i/products/21/thumbnail.png",
+    },
+    {
+    id: 22,
+    name: "Elbow Macaroni - 400 gm",
+    description: "Product details of Bake Parlor Big Elbow Macaroni - 400 gm",
+    price: 14,
+    manufacturer: "Bake Parlor Big",
+    category: "groceries",
+    imageUrl: "https://dummyjson.com/image/i/products/22/thumbnail.jpg",
+    },
+    {
+    id: 23,
+    name: "Orange Essence Food Flavou",
+    description: "Specifications of Orange Essence Food Flavour For Cakes and Baking Food Item",
+    price: 14,
+    manufacturer: "Baking Food Items",
+    category: "groceries",
+    imageUrl: "https://dummyjson.com/image/i/products/23/thumbnail.jpg",
+    },
+    {
+    id: 24,
+    name: "cereals muesli fruit nuts",
+    description: "original fauji cereal muesli 250gm box pack original fauji cereals muesli fruit nuts flakes breakfast cereal break fast faujicereals cerels cerel foji fouji",
+    price: 46,
+    manufacturer: "fauji",
+    category: "groceries",
+    imageUrl: "https://dummyjson.com/image/i/products/24/thumbnail.jpg",
+    },
+    {
+    id: 25,
+    name: "Gulab Powder 50 Gram",
+    description: "Dry Rose Flower Powder Gulab Powder 50 Gram • Treats Wounds",
+    price: 70,
+    manufacturer: "Dry Rose",
+    category: "groceries",
+    imageUrl: "https://dummyjson.com/image/i/products/25/thumbnail.jpg",
+    },
+    {
+    id: 26,
+    name: "Plant Hanger For Home",
+    description: "Boho Decor Plant Hanger For Home Wall Decoration Macrame Wall Hanging Shelf",
+    price: 41,
+    manufacturer: "Boho Decor",
+    category: "home-decoration",
+    imageUrl: "https://dummyjson.com/image/i/products/26/thumbnail.jpg",
+    },
+    {
+    id: 27,
+    name: "Flying Wooden Bird",
+    description: "Package Include 6 Birds with Adhesive Tape Shape: 3D Shaped Wooden Birds Material: Wooden MDF, Laminated 3.5mm",
+    price: 51,
+    manufacturer: "Flying Wooden",
+    category: "home-decoration",
+    imageUrl: "https://dummyjson.com/image/i/products/27/thumbnail.webp",
+    },
+    {
+    id: 28,
+    name: "3D Embellishment Art Lamp",
+    description: "3D led lamp sticker Wall sticker 3d wall art light on/off button cell operated (included)",
+    price: 20,
+    manufacturer: "LED Lights",
+    category: "home-decoration",
+    imageUrl: "https://dummyjson.com/image/i/products/28/thumbnail.jpg",
+    },
+    {
+    id: 29,
+    name: "Handcraft Chinese style",
+    description: "Handcraft Chinese style art luxury palace hotel villa mansion home decor ceramic vase with brass fruit plate",
+    price: 60,
+    manufacturer: "luxury palace",
+    category: "home-decoration",
+    imageUrl: "https://dummyjson.com/image/i/products/29/thumbnail.webp",
+    },
+    {
+    id: 30,
+    name: "Key Holder",
+    description: "Attractive DesignMetallic materialFour key hooksReliable & DurablePremium Quality",
+    price: 30,
+    manufacturer: "Golden",
+    category: "home-decoration",
+    imageUrl: "https://dummyjson.com/image/i/products/30/thumbnail.jpg",
     }
-]
+    ]
 
 app.use(express.json()); // replaces body parser in express 4.16 and later
 
@@ -38,8 +289,34 @@ const info = `
     -/ GET: search product with name, manufacturer and/or category
 </div>
 `
+
 app.get('/', (req, res) => {
     res.send(info);
+})
+
+app.get('/products/:id', (req, res) => {
+    const searchedProduct = products.find(p => p.id === req.params.id);
+    if (searchedProduct) {
+        res.json(searchedProduct);
+    }
+    else {
+        res.status(400).send({ error: 'Product not found'});
+    }
+})
+
+app.get('/products', (req, res) => {
+    let searchProducts = products;
+    if (req.query.name) {
+        searchProducts = searchProducts.filter(p => p.name.toLowerCase().includes(req.query.name.toLowerCase()));
+    }
+    if (req.query.manufacturer) {
+        searchProducts = searchProducts.filter(p => p.manufacturer.toLowerCase().includes(req.query.manufacturer.toLowerCase()));
+    }
+    if (req.query.category) {
+        searchProducts = searchProducts.filter(p => p.category.toLowerCase().includes(req.query.category.toLowerCase()));
+    }
+    console.log(req.query);
+    res.json(searchProducts);
 })
 
 app.post('/products', (req, res) => {
@@ -54,20 +331,6 @@ app.post('/products', (req, res) => {
     };
     products = products.concat(newProduct);
     res.json(products.find(p => p.id === newProduct.id));
-})
-
-app.get('/products/:id', (req, res) => {
-    const searchedProduct = products.find(p => p.id === req.params.id);
-    if (searchedProduct) {
-        res.json(searchedProduct);
-    }
-    else {
-        res.status(400).send({ error: 'Product not found'});
-    }
-})
-
-app.get('/products', (req, res) => {
-    res.json(products);
 })
 
 app.put('/products/:id', (req, res) => {
